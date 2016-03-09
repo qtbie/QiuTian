83a500cb7a7346373cb4306b51cbc1c30fdf6a7b
bqt <13654937421@163.com>
2016-03-09 11:14:56 +0800

    asd

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by bqt on 16/3/9.
+//  Copyright © 2016年 bqt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by bqt on 16/3/9.
+//  Copyright © 2016年 bqt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
