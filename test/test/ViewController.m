1a5b5c41211b0d40098bd5cb63dc40d82b47bb60
bqt <13654937421@163.com>
2016-03-09 12:53:07 +0800

    no message

--- test/test/ViewController.m
+++ test/test/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *titlle;
+
 
 @end
 
@@ -17,6 +19,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.navigationController.title = @"asdads";
+    NSArray *arr = [[NSBundle mainBundle] loadNibNamed:@"lable" owner:self options:nil];
+    UIView * view = arr[0];
+    [self.view addSubview:view];
+    
+    NSArray *consw = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|-200-[view]-20-|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(view)];
+    NSArray *consh = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-300-[view(50)]" options:0 metrics:nil views:NSDictionaryOfVariableBindings(view)];
+    [NSLayoutConstraint activateConstraints:consh];
+    [NSLayoutConstraint activateConstraints:consw];
+    [view addConstraints:consw];
+    [view addConstraints:consh];
+    //view.frame = CGRectMake(0, 100, 300, 100);
+    
+//    self.titlle.text = self.tabBarController;
 }
 
 - (void)didReceiveMemoryWarning {
